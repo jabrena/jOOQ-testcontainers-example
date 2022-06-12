98b426c1c6a03b7d356a27b55339e886553c7cc3
Juan Antonio Breña Moral <bren@juanantonio.info>
2022-06-12 13:14:40 +0200

    Initial setup

---
+++ src/main/java/org/jooq/example/JooqQuickstartApplication.java
@@ -0,0 +1,13 @@
+package org.jooq.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JooqQuickstartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JooqQuickstartApplication.class, args);
+    }
+
+}
